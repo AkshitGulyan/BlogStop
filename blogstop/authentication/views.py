d46b23a3a7148abf5df4708ea4c39f231e8c2e67
Akshit Gulyan <akshitgulyan@gmail.com>
2023-09-08 23:23:55 +0530

    Activation Mail code added

--- blogstop/authentication/views.py
+++ blogstop/authentication/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from .tokens import generate_token
+
+
+from blogstop import settings
 
 def home(request):
     return render(request, 'authentication/index.html')
@@ -19,28 +28,48 @@ def signup(request):
 
         if User.objects.filter(username=username):
             messages.error(request, 'Username already in use')
-            return redirect('home')
+            return redirect('signup')
         
         if User.objects.filter(email=email):
             messages.error(request, 'E-Mail already registered')
-            return redirect('home')
+            return redirect('signup')
         
         if pass1!=pass2:
             messages.error(request,"Passwords didn't match")
-            return redirect('home')
+            return redirect('signup')
         
         if not username.isalnum():
             messages.error(request, 'Username should be alphanueric in nature')
-            return redirect('home')
+            return redirect('signup')
         
 
         
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
+        myuser.isactive = False
         myuser.save()
 
         messages.success(request, "Your account has been successfully created!")
+
+        #welcome mail
+        subject = 'Welcome to BlogStop'
+        message = "Hello "+ myuser.first_name + "!, It's nice to have you\n Thanks for registering with us!" 
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently= True)
+
+        #confirmation mail
+        current_site = get_current_site(request)
+        email_subject = 'Activate Your Account for BlogStop'
+        email_message = render_to_string('email_confirmation.html', {
+            'name' = myuser.first_name,
+            'domain' = current_site.domain,
+            'uid' = urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token' = generate_token.make_token(myuser)
+        })
+
+
 
         return redirect('signin')
 
