e21a627425e04b23ffce3e601bdbf357752b34f9
Akshit Gulyan <akshitgulyan@gmail.com>
2023-09-08 22:51:18 +0530

    Mail API Integration

--- blogstop/authentication/views.py
+++ blogstop/authentication/views.py
@@ -16,6 +16,22 @@ def signup(request):
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
+
+        if User.objects.filter(username=username):
+            messages.error(request, 'Username already in use')
+            return redirect('home')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, 'E-Mail already registered')
+            return redirect('home')
+        
+        if pass1!=pass2:
+            messages.error(request,"Passwords didn't match")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, 'Username should be alphanueric in natue')
+
         
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
