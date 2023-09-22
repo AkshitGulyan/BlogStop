4b45791568a96157dad348eb283915080e89e5df
Akshit Gulyan <akshitgulyan@gmail.com>
2023-09-23 01:01:44 +0530

    someTweaks

--- blogstop/blog/urls.py
+++ blogstop/blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .views import Blogview
+from django.urls import path
+from .views import Blogview 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('blog', views.home, name='home'),
+    path("/blog", Blogview.as_view(), name="blog")
 ]
