01b3edd1aedb0a6afcff94d49ed320c1d54b9be5
Akshit Gulyan <akshitgulyan@gmail.com>
2023-10-04 22:24:20 +0530

    detailed view added

--- blogstop/blog/urls.py
+++ blogstop/blog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Blogview 
+from .views import Blogview , Detailedview
 
 urlpatterns = [
     path("blog", Blogview.as_view(), name="blog"),
+    path('article/<int:pk>', Detailedview.as_view(), name='article'),
 ]
--- blogstop/blog/views.py
+++ blogstop/blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 class Blogview(ListView):
     model = Post
     template_name = 'blog/blog.html'
+
+class Detailedview(DetailView):
+    model=Post
+    template_name = 'blog/blogdetail.html'
